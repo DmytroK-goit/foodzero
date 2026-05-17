6aadd7843b8e455ba5f11282d890c7b7a42076ec
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2026-05-17 17:04:09 +0300

    add contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import contactbg from "../../../img/contact/contactbg.png";
 export default function Page() {
   return (
-    <section>
-      <p>Contact</p>
+    <section className="bg-white w-full overflow-hidden">
+      <section className="relative h-screen flex items-center justify-center px-[138px]">
+        <Image
+          src={contactbg}
+          alt="Contact us Background"
+          fill
+          priority
+          className="object-cover"
+        />
+
+        <div className="relative z-10 text-center max-w-[1100px]">
+          <h2 className="text-white text-[108px] leading-[1] mb-8">
+            Get in Touch
+          </h2>
+
+          <p className="text-white text-[26px] leading-[1.8] opacity-90 max-w-[760px] mx-auto">
+            The freshest ingredients for you every day
+          </p>
+        </div>
+      </section>
     </section>
   );
 }
